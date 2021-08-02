c1a91107e22902341a133ff873765eb9c5f211d6
Oleg Dreyman <odreyman@surprisehr.com>
2021-08-02 12:31:49 +0300

    Initial Commit

---
+++ PostsDemo/PostsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostsDemoApp.swift
+//  PostsDemo
+//
+//  Created by Oleg Dreyman on 02.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct PostsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
