41ec8eccc8cb82d5cad3ee8742b54132ea19d931
Oleg Dreyman <odreyman@surprisehr.com>
2021-08-04 15:01:12 +0300

    refactor: slightly better refresh control

--- PostsDemo/Services/PostsRepo.swift
+++ PostsDemo/Services/PostsRepo.swift
@@ -23,22 +23,44 @@ final class PostsRepo: ObservableObject {
 
     enum State {
         case idle
-        case loading
+        case loading(Sourceful<[Post]>?)
         case loaded(Sourceful<[Post]>)
         case failed(Error)
+
+        var existing: Sourceful<[Post]>? {
+            switch self {
+            case .loading(let posts):
+                return posts
+            case .loaded(let posts):
+                return posts
+            case .idle, .failed:
+                return nil
+            }
+        }
+
+        var isLoading: Bool {
+            if case .loading = self {
+                return true
+            }
+            return false
+        }
     }
 
     @Published var state: State = .idle
 
-    func fetchPosts(transitioningToLoading: Bool = true) {
+    func fetchPosts(transitioningToLoading: Bool = true, useCache: Bool = true) {
         if transitioningToLoading {
-            state = .loading
+            state = .loading(state.existing)
         }
 
         let apiCall = api.getPostsWithUsers()
             .map({ $0.map(Post.init(postWithUser:)) })
 
-        cache.connecting(to: apiCall)
+        let finalCall: AnyPublisher<Sourceful<[Post]>, Error> = useCache ?
+            cache.connecting(to: apiCall) :
+            apiCall.map({ Sourceful(value: $0, source: .latest) }).eraseToAnyPublisher()
+
+        finalCall
             .map({ State.loaded($0) })
             .catch({ Just(State.failed($0)) })
             .assign(to: &$state)
--- PostsDemo/Views/Posts/PostsContainerView.swift
+++ PostsDemo/Views/Posts/PostsContainerView.swift
@@ -16,12 +16,23 @@ struct PostsContainerView: View {
         switch repo.state {
         case .idle:
             Color.clear.onAppear(perform: fetch)
-        case .loading:
-            ProgressView()
+        case .loading(let existing):
+            if let existing = existing {
+                ZStack {
+                    PostsView(posts: existing, isPullToRefreshShowing: .constant(true), onRefresh: { })
+                        .opacity(0.2)
+                    ProgressView()
+                }
+            } else {
+                ProgressView()
+            }
         case .loaded(let posts):
             PostsView(posts: posts, isPullToRefreshShowing: $isPullToRefreshShowing) {
                 self.refresh()
             }
+            .opacity(posts.source == .latest ? 1.0 : 0.2)
+            .disabled(posts.source == .cache)
+            .onAppear { self.isPullToRefreshShowing = false }
         case .failed(let error):
             Text(String(describing: error))
         }
@@ -32,7 +43,7 @@ struct PostsContainerView: View {
     }
 
     func refresh() {
-        repo.fetchPosts(transitioningToLoading: true)
+        repo.fetchPosts(transitioningToLoading: true, useCache: false)
     }
 }
 
